d6e6a11e0f4c40632ac9ccf45863d9bde4c97dff
terfiel-22 <dietherfiel@gmail.com>
2025-09-04 01:22:12 +0800

    Add pool section in Amenities

--- views/guest/amenities.view.php
+++ views/guest/amenities.view.php
@@ -19,7 +19,95 @@
     <!-- Header Section Start -->
     <?php view("guest/partials/header.partial.php") ?>
 
-    <!-- BODY HERE -->
+    <!-- Breadcrumb Section Start -->
+    <?php view("guest/partials/breadcrumb.partial.php", [
+        'pageName' => "Amenities"
+    ]) ?>
+
+    <!-- GT Pool Section Start -->
+    <section class="gt-why-choose-us-section-2 section-padding fix">
+        <div class="gt-choose-us-shape">
+            <img src="/assets/guest/img/home-2/choose-us/Vector-01.png" alt="img" />
+        </div>
+        <div class="container">
+            <div class="gt-section-title-area">
+                <div class="gt-section-title">
+                    <h6 class="wow fadeInUp">
+                        <img src="/assets/guest/img/sub-left.svg" alt="img" />
+                        Dive into relaxation
+                    </h6>
+                    <h2 class="wow fadeInUp" data-wow-delay=".2s">
+                        Pools
+                    </h2>
+                </div>
+            </div>
+            <div class="row">
+                <div
+                    class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp"
+                    data-wow-delay=".3s">
+                    <div class="gt-why-choose-us-images">
+                        <div class="gt-choose-us-image">
+                            <img
+                                src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg"
+                                alt="img" />
+                            <div class="gt-content">
+                                <h3>Enjoy the Adult Pool</h3>
+                                <p>
+                                    Take a refreshing dip in our adult pool,
+                                    designed for relaxation and recreation.
+                                    With ample space for swimming and
+                                    lounging, it's the perfect spot to soak up
+                                    the sun and enjoy a leisurely afternoon.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp"
+                    data-wow-delay=".5s">
+                    <div class="gt-why-choose-us-images">
+                        <div class="gt-choose-us-image">
+                            <img
+                                src="/assets/guest/img/home-2/choose-us/choose-us-02.jpg"
+                                alt="img" />
+                            <div class="gt-content">
+                                <h3>Fun for the Little Ones</h3>
+                                <p>
+                                    Our kiddie pool is designed with safety
+                                    and fun in mind. With shallow water and
+                                    fountain, it&apos;s a perfect place for out little
+                                    ones to splash and play.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div
+                    class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp"
+                    data-wow-delay=".7s">
+                    <div class="gt-why-choose-us-images">
+                        <div class="gt-choose-us-image">
+                            <img
+                                src="/assets/guest/img/home-2/choose-us/choose-us-03.jpg"
+                                alt="img" />
+                            <div class="gt-content">
+                                <h3>Relax in Our Jacuzzi</h3>
+                                <p>
+                                    Indulge in the soothing warmth of our
+                                    jacuzzi. Perfect for unwinding after a long
+                                    day, it offers a tranquil escape with its
+                                    massaging jets and serene ambiance.
+                                    Experience pure bliss as you let your
+                                    worries melt away.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
     <!-- GT Footer Section Start -->
     <?php view("guest/partials/footer.partial.php") ?>
