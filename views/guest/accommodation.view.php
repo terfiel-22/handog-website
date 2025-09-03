4b60631b09e99e733ff1faa21408afbf7a225b7a
terfiel-22 <dietherfiel@gmail.com>
2025-09-03 17:33:06 +0800

    Update Accommodation UI

--- views/guest/accommodation.view.php
+++ views/guest/accommodation.view.php
@@ -333,6 +333,117 @@
         </div>
     </section>
 
+    <!-- GT Exclusive Section Start -->
+    <section class="gt-service-section fix section-padding section-bg-3">
+        <div class="left-shape">
+            <img src="/assets/guest/img/home-3/service/left-shape.png" alt="img">
+        </div>
+        <div class="container">
+            <div class="gt-service-wrapper-3">
+                <div class="row g-4">
+                    <div class="col-lg-6">
+                        <div class="swiper service-image-slider">
+                            <div class="swiper-wrapper">
+                                <div class="swiper-slide">
+                                    <div class="service-image">
+                                        <img src="/assets/guest/img/home-3/service/service-01.jpg" alt="img">
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="service-image">
+                                        <img src="/assets/guest/img/home-3/service/service-01.jpg" alt="img">
+                                    </div>
+                                </div>
+                                <div class="swiper-slide">
+                                    <div class="service-image">
+                                        <img src="/assets/guest/img/home-3/service/service-01.jpg" alt="img">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="array-button-2 justify-content-center">
+                                <button class="array-next"><i class="fa-solid fa-chevron-left"></i></button>
+
+                                <button class="array-prev"><i class="fa-solid fa-chevron-right"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="service-content">
+                            <div class="gt-section-title mb-0">
+                                <h6 class="wow fadeInUp">
+                                    <img src="/assets/guest/img/sub-left.svg" alt="img">
+                                    Privacy Beyond Compare
+                                </h6>
+                                <h2 class="wow fadeInUp" data-wow-delay=".2s">
+                                    Exclusive
+                                </h2>
+                            </div>
+                            <p class="service-text wow fadeInUp" data-wow-delay=".4s">
+                                Make the entire resort your own private paradise!
+                                With our exclusive reservation package, you get full,
+                                private access to all our amenities. Perfect for
+                                reunions, celebrations, or peaceful getaways. No
+                                crowds, no interruptions just you and your loved ones
+                                enjoying the moment.
+                            </p>
+                            <div class="wow fadeInUp" data-wow-delay=".5s">
+                                <h3>
+                                    What&apos;s included in your exclusive reservation?
+                                </h3>
+                                <ul class="check-list">
+                                    <li>
+                                        <i class="fa-solid fa-circle-check"></i>
+                                        Swimming pool
+                                    </li>
+                                    <li>
+                                        <i class="fa-solid fa-circle-check"></i>
+                                        Cottages
+                                    </li>
+                                    <li>
+                                        <i class="fa-solid fa-circle-check"></i>
+                                        3 Standard Rooms (ideal for solo guests or couples)
+                                    </li>
+                                    <li>
+                                        <i class="fa-solid fa-circle-check"></i>
+                                        1 Family Room (perfect for 2 to 4 guests)
+                                    </li>
+                                    <li>
+                                        <i class="fa-solid fa-circle-check"></i>
+                                        Event hall
+                                    </li>
+                                    <li>
+                                        <i class="fa-solid fa-circle-check"></i>
+                                        Sound System
+                                    </li>
+                                    <li>
+                                        <i class="fa-solid fa-circle-check"></i>
+                                        Open relaxing spaces and resort grounds
+                                    </li>
+                                </ul>
+                            </div>
+                            <div class="wow fadeInUp my-2" data-wow-delay=".6s">
+                                <h3>
+                                    How much does it cost to rent the entire resort?
+                                </h3>
+                                <ul class="check-list">
+                                    <li>
+                                        <i class="fa-solid fa-circle-check"></i>
+                                        55,000 Pesos for 12 hrs
+                                    </li>
+                                    <li>
+                                        <i class="fa-solid fa-circle-check"></i>
+                                        80,000 pesos for 24 hrs
+                                    </li>
+                                </ul>
+                            </div>
+                            <a href="/" class="gt-theme-btn wow fadeInUp" data-wow-delay=".9s">BOOK NOW</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <!-- GT Footer Section Start -->
     <?php view("guest/partials/footer.partial.php") ?>
 
