5669ec3231f3cc7e4a2fc09942b993958ecb94cd
terfiel-22 <dietherfiel@gmail.com>
2025-09-11 00:28:45 +0800

    Refactor other sections

---
+++ views/admin/partials/breadcrumb.partial.php
@@ -0,0 +1,13 @@
+<div class="d-flex flex-wrap align-items-center justify-content-between gap-3 mb-24">
+    <h6 class="fw-semibold mb-0">Dashboard</h6>
+    <ul class="d-flex align-items-center gap-2">
+        <li class="fw-medium">
+            <a href="/admin" class="d-flex align-items-center gap-1 hover-text-primary">
+                <iconify-icon icon="solar:home-smile-angle-outline" class="icon text-lg"></iconify-icon>
+                Dashboard
+            </a>
+        </li>
+        <li>-</li>
+        <li class="fw-medium">AI</li>
+    </ul>
+</div>
--- views/admin/partials/sidebar.partial.php
+++ views/admin/partials/sidebar.partial.php
@@ -3,7 +3,7 @@
         <iconify-icon icon="radix-icons:cross-2"></iconify-icon>
     </button>
     <div>
-        <a href="index.html" class="sidebar-logo">
+        <a href="/admin" class="sidebar-logo">
             <img src="/assets/admin/images/logo.png" alt="site logo" class="light-logo">
             <img src="/assets/admin/images/logo-light.png" alt="site logo" class="dark-logo">
             <img src="/assets/admin/images/logo-icon.png" alt="site logo" class="logo-icon">
