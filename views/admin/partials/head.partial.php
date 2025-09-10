2af03e6644e2ff44d2787a37825c0fdf1ffb06e0
terfiel-22 <dietherfiel@gmail.com>
2025-09-10 17:16:46 +0800

    Update Admin Dashboard

---
+++ views/admin/partials/head.partial.php
@@ -0,0 +1,30 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="icon" type="image/png" href="/assets/admin/images/favicon.png" sizes="16x16">
+    <!-- remix icon font css  -->
+    <link rel="stylesheet" href="/assets/admin/css/remixicon.css">
+    <!-- BootStrap css -->
+    <link rel="stylesheet" href="/assets/admin/css/lib/bootstrap.min.css">
+    <!-- Apex Chart css -->
+    <link rel="stylesheet" href="/assets/admin/css/lib/apexcharts.css">
+    <!-- Data Table css -->
+    <link rel="stylesheet" href="/assets/admin/css/lib/dataTables.min.css">
+    <!-- Text Editor css -->
+    <link rel="stylesheet" href="/assets/admin/css/lib/editor-katex.min.css">
+    <link rel="stylesheet" href="/assets/admin/css/lib/editor.atom-one-dark.min.css">
+    <link rel="stylesheet" href="/assets/admin/css/lib/editor.quill.snow.css">
+    <!-- Date picker css -->
+    <link rel="stylesheet" href="/assets/admin/css/lib/flatpickr.min.css">
+    <!-- Calendar css -->
+    <link rel="stylesheet" href="/assets/admin/css/lib/full-calendar.css">
+    <!-- Vector Map css -->
+    <link rel="stylesheet" href="/assets/admin/css/lib/jquery-jvectormap-2.0.5.css">
+    <!-- Popup css -->
+    <link rel="stylesheet" href="/assets/admin/css/lib/magnific-popup.css">
+    <!-- Slick Slider css -->
+    <link rel="stylesheet" href="/assets/admin/css/lib/slick.css">
+    <!-- main css -->
+    <link rel="stylesheet" href="/assets/admin/css/style.css">
+</head>
---
+++ views/admin/partials/plugins.partial.php
@@ -0,0 +1,22 @@
+    <!-- jQuery library js -->
+    <script src="/assets/admin/js/lib/jquery-3.7.1.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="/assets/admin/js/lib/bootstrap.bundle.min.js"></script>
+    <!-- Apex Chart js -->
+    <script src="/assets/admin/js/lib/apexcharts.min.js"></script>
+    <!-- Data Table js -->
+    <script src="/assets/admin/js/lib/dataTables.min.js"></script>
+    <!-- Iconify Font js -->
+    <script src="/assets/admin/js/lib/iconify-icon.min.js"></script>
+    <!-- jQuery UI js -->
+    <script src="/assets/admin/js/lib/jquery-ui.min.js"></script>
+    <!-- Vector Map js -->
+    <script src="/assets/admin/js/lib/jquery-jvectormap-2.0.5.min.js"></script>
+    <script src="/assets/admin/js/lib/jquery-jvectormap-world-mill-en.js"></script>
+    <!-- Popup js -->
+    <script src="/assets/admin/js/lib/magnifc-popup.min.js"></script>
+    <!-- Slick Slider js -->
+    <script src="/assets/admin/js/lib/slick.min.js"></script>
+    <!-- main js -->
+    <script src="/assets/admin/js/app.js"></script>
+    <script src="/assets/admin/js/homeOneChart.js"></script>
