766e34d6f2ead256d2358f25fce094c6f871e91c
terfiel-22 <dietherfiel@gmail.com>
2025-09-12 17:18:51 +0800

    Create Facility Model

---
+++ Http/Models/Facility.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Http\Models;
+
+use Core\App;
+use Core\Database;
+
+class Facility
+{
+    protected Database $db;
+
+    public function __construct()
+    {
+        $this->db = App::resolve(Database::class);
+    }
+
+    public function createFacility($attributes)
+    {
+        $this->db->query(
+            "INSERT INTO facilities(name, type, description, capacity, price, amenities, status) VALUES(:name, :type, :description, :capacity, :price, :amenities, :status)",
+            $attributes
+        );
+    }
+
+    public function fetchFacilityById($id)
+    {
+        return $this->db->query('SELECT * FROM facilities WHERE id=:id', compact('id'))->find();
+    }
+
+    public function fetchFacilities()
+    {
+        return $this->db->query('SELECT * FROM facilities')->get();
+    }
+
+    public function fetchFacilitiesByType($type)
+    {
+        return $this->db->query('SELECT * FROM facilities WHERE type=:type', compact('type'))->get();
+    }
+
+    public function deleteFacility($id)
+    {
+        $this->db->query(
+            "DELETE FROM facilities WHERE id = :id",
+            compact('id')
+        );
+    }
+}
