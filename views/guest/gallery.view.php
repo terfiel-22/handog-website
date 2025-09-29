1f4ccc8832dffd402cdb62a5131bbefe4a754d12
terfiel-22 <dietherfiel@gmail.com>
2025-09-30 00:56:19 +0800

    Update Gallery Page

--- views/guest/gallery.view.php
+++ views/guest/gallery.view.php
@@ -41,134 +41,32 @@
                     </h2>
                 </div>
             </div>
-            <div class="row">
-                <div
-                    class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp"
-                    data-wow-delay=".3s">
-                    <div class="gt-why-choose-us-images">
-                        <div class="gt-choose-us-image">
-                            <img
-                                src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg"
-                                alt="img" />
-                            <div class="gt-content">
-                                <h3>Enjoy the Adult Pool</h3>
-                                <p>
-                                    Take a refreshing dip in our adult pool,
-                                    designed for relaxation and recreation.
-                                    With ample space for swimming and
-                                    lounging, it's the perfect spot to soak up
-                                    the sun and enjoy a leisurely afternoon.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+            <div class="row g-3 wow fadeInUp">
+                <!-- Example images -->
+                <div class="col-6 col-md-4 col-lg-3">
+                    <img src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg" class="img-fluid gallery-img clickable-img" alt="Beach" data-index="0">
                 </div>
-                <div
-                    class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp"
-                    data-wow-delay=".5s">
-                    <div class="gt-why-choose-us-images">
-                        <div class="gt-choose-us-image">
-                            <img
-                                src="/assets/guest/img/home-2/choose-us/choose-us-02.jpg"
-                                alt="img" />
-                            <div class="gt-content">
-                                <h3>Fun for the Little Ones</h3>
-                                <p>
-                                    Our kiddie pool is designed with safety
-                                    and fun in mind. With shallow water and
-                                    fountain, it&apos;s a perfect place for out little
-                                    ones to splash and play.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <img src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg" class="img-fluid gallery-img clickable-img" alt="Pool" data-index="1">
                 </div>
-                <div
-                    class="col-xl-4 col-lg-6 col-md-6 wow fadeInUp"
-                    data-wow-delay=".7s">
-                    <div class="gt-why-choose-us-images">
-                        <div class="gt-choose-us-image">
-                            <img
-                                src="/assets/guest/img/home-2/choose-us/choose-us-03.jpg"
-                                alt="img" />
-                            <div class="gt-content">
-                                <h3>Relax in Our Jacuzzi</h3>
-                                <p>
-                                    Indulge in the soothing warmth of our
-                                    jacuzzi. Perfect for unwinding after a long
-                                    day, it offers a tranquil escape with its
-                                    massaging jets and serene ambiance.
-                                    Experience pure bliss as you let your
-                                    worries melt away.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <img src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg" class="img-fluid gallery-img clickable-img" alt="Restaurant" data-index="2">
                 </div>
-            </div>
-        </div>
-    </section>
-
-    <!-- GT Instagram Section Start -->
-    <section class="gt-instagram-section fix">
-        <div class="gt-instagram-wrapper-2">
-            <div class="swiper gt-instagram-slider-2">
-                <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="gt-instagram-image mt-0">
-                            <img src="/assets/guest/img/home-2/instagram/01.jpg" alt="img" />
-                            <a href="index.html" class="gt-icon">
-                                <i class="fa-brands fa-instagram"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="gt-instagram-image mt-0">
-                            <img src="/assets/guest/img/home-2/instagram/02.jpg" alt="img" />
-                            <a href="index.html" class="gt-icon">
-                                <i class="fa-brands fa-instagram"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="gt-instagram-image mt-0">
-                            <img src="/assets/guest/img/home-2/instagram/03.jpg" alt="img" />
-                            <a href="index.html" class="gt-icon">
-                                <i class="fa-brands fa-instagram"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="gt-instagram-image mt-0">
-                            <img src="/assets/guest/img/home-2/instagram/04.jpg" alt="img" />
-                            <a href="index.html" class="gt-icon">
-                                <i class="fa-brands fa-instagram"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="gt-instagram-image mt-0">
-                            <img src="/assets/guest/img/home-2/instagram/05.jpg" alt="img" />
-                            <a href="index.html" class="gt-icon">
-                                <i class="fa-brands fa-instagram"></i>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="gt-instagram-image mt-0">
-                            <img src="/assets/guest/img/home-2/instagram/06.jpg" alt="img" />
-                            <a href="index.html" class="gt-icon">
-                                <i class="fa-brands fa-instagram"></i>
-                            </a>
-                        </div>
-                    </div>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <img src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg" class="img-fluid gallery-img clickable-img" alt="Rooms" data-index="3">
                 </div>
-            </div>
-            <div class="gt-instagram-text-box">
-                <div class="icon">
-                    <i class="fa-brands fa-instagram"></i>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <img src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg" class="img-fluid gallery-img clickable-img" alt="Beach" data-index="0">
                 </div>
-                Follow Us On Instagram
+                <div class="col-6 col-md-4 col-lg-3">
+                    <img src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg" class="img-fluid gallery-img clickable-img" alt="Pool" data-index="1">
+                </div>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <img src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg" class="img-fluid gallery-img clickable-img" alt="Restaurant" data-index="2">
+                </div>
+                <div class="col-6 col-md-4 col-lg-3">
+                    <img src="/assets/guest/img/home-2/choose-us/choose-us-01.jpg" class="img-fluid gallery-img clickable-img" alt="Rooms" data-index="3">
+                </div>
             </div>
         </div>
     </section>
