24a85ef937474e4b2f67cadd975588dd960f2d8f
terfiel-22 <dietherfiel@gmail.com>
2025-10-27 14:34:17 +0800

    Delete Promo

--- Http/controllers/admin/promo/destroy.controller.php
+++ Http/controllers/admin/promo/destroy.controller.php
@@ -1,17 +1,10 @@
 <?php
 
 use Core\App;
-use Core\FileUploadHandler;
-use Http\Models\Amenity;
+use Http\Models\Promo;
 
-$amenity = App::resolve(Amenity::class)->fetchSingleAmenityWithImagesById($_POST["item_id"]);
+$promo = App::resolve(Promo::class)->fetchPromoById($_POST["item_id"]);
 
-// Delete Image Files
-$images = explode(',', $amenity["images"]);
-foreach ($images as $image) {
-    App::resolve(FileUploadHandler::class)->deleteFile($image);
-}
+App::resolve(Promo::class)->deletePromo($promo["id"]);
 
-App::resolve(Amenity::class)->deleteAmenity($amenity["id"]);
-
-redirect("/admin/amenities");
+redirect("/admin/promos");
