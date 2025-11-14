f7b205c2a29dc4df68d84379bab9b76eec4e1b31
terfiel-22 <dietherfiel@gmail.com>
2025-11-14 16:51:01 +0800

    Delete uploaded ID when the reservation is deleted

--- Http/controllers/admin/reservation/destroy.controller.php
+++ Http/controllers/admin/reservation/destroy.controller.php
@@ -1,9 +1,20 @@
 <?php
 
 use Core\App;
+use Core\FileUploadHandler;
 use Http\Models\Reservation;
+use Http\Models\ReservationGuest;
 
 $reservation = App::resolve(Reservation::class)->fetchReservationById($_POST["item_id"]);
+
+$reservationGuests = App::resolve(ReservationGuest::class)->fetchGuestsByReservationId($reservation["id"]);
+
+// Delete uploaded IDs
+foreach ($reservationGuests as $guest) {
+    if (isset($guest["presented_id"])) {
+        App::resolve(FileUploadHandler::class)->deleteFile($guest["presented_id"]);
+    }
+}
 
 App::resolve(Reservation::class)->deleteReservation($reservation["id"]);
 
